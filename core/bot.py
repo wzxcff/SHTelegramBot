605e4c32a1eadb598af2d88bc68fe6013b3faaa2
wzxcff <Shevasfloyd@gmail.com>
2025-09-03 23:33:26 +0200

    Base structure

--- core/bot.py
+++ core/bot.py
@@ -1,15 +1,25 @@
 import telebot
 import os
 from dotenv import load_dotenv
+from handlers import *
 
 load_dotenv()
 
 bot = telebot.TeleBot(os.getenv('TOKEN'))
 
+functions_map = {
+    "hello": hello_handler
+}
+
 
 @bot.message_handler(content_types=['text'])
 def message_handler(message):
-    bot.send_message(message.chat.id, message.text)
+    try:
+        bot.send_message(message.chat.id, functions_map[str(message.text).lower()](message))
+    except KeyError:
+        bot.send_message(message.chat.id, "No function mapped to this word(s).")
+    except Exception as e:
+        print(f"Error occurred:\n{e}\n------------------")
 
 
 bot.polling(none_stop=True)
---
+++ core/handlers.py
@@ -0,0 +1,2 @@
+def hello_handler(message):
+    return f"Hello, my friend {message.from_user.first_name}!"
